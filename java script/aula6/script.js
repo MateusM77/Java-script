980b538c779a128512e5ebaa662e085de873f181
Mateus Verissimo <112414387+MateusM77@users.noreply.github.com>
2022-08-29 21:51:13 +0100

    subindo java script

---
+++ java script/aula6/script.js
@@ -0,0 +1,21 @@
+
+// var, let, const
+
+
+// string
+// var nome = 'Mateus' 
+
+// number
+// var idade = '35'
+
+
+// let nome = 'Mateus'
+
+
+const _NOME_ = 'Mateus'
+
+
+
+
+
+console.log(nome)
---
+++ java script/aula9/script.js
@@ -0,0 +1,16 @@
+
+let nome = prompt('Digite seu nome')
+
+if (nome === 'Mateus' || nome === 'mateus') {
+  document.write(`Olá ${nome}, seja bem vindo!`)
+} else {
+  document.write('Você não tem acesso de administrador!')
+}
+
+
+
+
+
+
+
+
